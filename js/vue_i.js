393c11330029b8ccc9d0e8c0ea32d72083d65a8b
Asbeeq <assylbek.zakrikchinov@gmail.com>
2020-05-06 02:04:15 +0600

    Add files via upload

---
+++ js/vue_i.js
@@ -0,0 +1,12 @@
+var  vm1 = new Vue ({
+   el: '#vue_det',
+   data: {
+      img : "img/1.PNG",
+      img2 : "img/2.PNG"
+   },
+   // methods: {
+   //    mydetails : function() {
+   //       //return "I am "+this.firstname +" "+ this.lastname+" "+ this.address;
+   //    }
+   // }
+})
